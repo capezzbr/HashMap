e554f964b3b9871768a67a782bd0fc40590130a0
Bruno Capezzali <capezzbr@gmail.com>
2014-05-06 16:52:31 +0200

    fixed null value as key

--- HashMap/src/hashmap/HashMap.java
+++ HashMap/src/hashmap/HashMap.java
@@ -5,110 +5,116 @@ import java.util.LinkedList;
 
 /**
  * @author capezzbr
- * @param <K> is the key type 
+ * @param <K> is the key type
  * @param <V> is the value type
  */
 public class HashMap<K, V> {
-
-    class Item<K, V> {
-        private final K key; 
-        private V value;
-
-        public Item(K key, V value) { 
-           this.key = key; this.value = value; 
-        }
-
-        public K getKey() { 
-           return this.key; 
-        }
-        
-        public V getValue() { 
-           return this.value; 
-        }
-        
-        public void setValue(V value) { 
-           this.value = value; 
-        }
-        
-        public boolean hasSameKey(K key) { 
-           return this.key.equals(key); 
-        }
-    }
-
-    private final static int DEFAULT_SIZE = 20;
-    private final int size;
-    private final LinkedList<Item<K, V>>[] elements;
-
+   
+   class Item<K, V> {
+      private final K key;
+      private V value;
+      
+      public Item(K key, V value) {
+         this.key = key; this.value = value;
+      }
+      
+      public K getKey() {
+         return this.key;
+      }
+      
+      public V getValue() {
+         return this.value;
+      }
+      
+      public void setValue(V value) {
+         this.value = value;
+      }
+      
+      public boolean hasSameKey(K key) {
+         return this.key == null ? this.key == key : this.key.equals(key);
+      }
+   }
+   
+   private final static int DEFAULT_SIZE = 20;
+   private final int size;
+   private final LinkedList<Item<K, V>>[] elements;
+   
    public HashMap() {
       this(DEFAULT_SIZE);
    }
-    
-    public HashMap(int size) {
-        this.size = size;
-        this.elements = (LinkedList<Item<K, V>>[])new LinkedList[size];
-    }
-
-    private int hashForKey(K key) {
-        return key.toString().length() % size;
-    }
-
-    public boolean hasElement(K key) {
-       int index = hashForKey(key);
-       if ( elements[index] == null ) {
-          return false;
-       }
-       
-       for ( Item<K, V> i : elements[index] ) {
-          if ( i.hasSameKey(key) ) {
-             return true;
-          }
-       }
-       return false;
-    }
-
-    public V get(K key) {
-        int index = hashForKey(key);
-        if ( elements[index] == null ) {
-           return null;
-        }
-
-        for ( Item<K, V> i : elements[index] ) {
-            if ( i.hasSameKey(key) ) {
-                return i.getValue();
-            }
-        }
-        return null;
-    }
-
-    public void put(K key, V value) {
-        int index = hashForKey(key);
-        if ( elements[index] == null ) {
-            elements[index] = new LinkedList<Item<K, V>>();
-        }
-
-        for ( Item<K, V> i : elements[index] ) {
-            if ( i.hasSameKey(key) ) {
-                elements[index].remove(i);
-                break;
-            }
-        }
-
-        elements[index].add( new Item<K, V>(key, value) );
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for ( int k = 0; k < size; k++ ) {
+   
+   public HashMap(int size) {
+      this.size = size;
+      this.elements = (LinkedList<Item<K, V>>[])new LinkedList[size];
+   }
+   
+   private int hashForKey(K key) {
+      return key == null ? 0 : key.toString().length() % size;
+   }
+   
+   public boolean hasElement(K key) {
+      int index = hashForKey(key);
+      if ( elements[index] == null ) {
+         return false;
+      }
+      
+      for ( Item<K, V> i : elements[index] ) {
+         if ( i.hasSameKey(key) ) {
+            return true;
+         }
+      }
+      return false;
+   }
+   
+   public V get(K key) {
+      int index = hashForKey(key);
+      if ( elements[index] == null ) {
+         return null;
+      }
+      
+      for ( Item<K, V> i : elements[index] ) {
+         if ( i.hasSameKey(key) ) {
+            return i.getValue();
+         }
+      }
+      return null;
+   }
+   
+   public void put(K key, V value) {
+      int index = hashForKey(key);
+      if ( elements[index] == null ) {
+         elements[index] = new LinkedList<Item<K, V>>();
+      }
+      
+      for ( Item<K, V> i : elements[index] ) {
+         if ( i.hasSameKey(key) ) {
+            elements[index].remove(i);
+            break;
+         }
+      }
+      
+      elements[index].add( new Item<K, V>(key, value) );
+   }
+   
+   @Override
+   public String toString() {
+      StringBuilder sb = new StringBuilder();
+      int elementIndex;
+      for ( int k = 0; k < size; k++ ) {
+         if ( elements[k] != null ) {
+            elementIndex = 0;
             sb.append("[").append(k).append("] -> {");
             for ( Item<K, V> i : elements[k] ) {
-                sb.append(i.getKey()).append(" = ").append(i.getValue()).append(", ");
+               sb.append(i.getKey()).append(" = ").append(i.getValue());
+               if ( elementIndex < elements[k].size() -1 ) {
+                  sb.append(", ");
+               }
+               elementIndex++;
             }
-        }
-        return sb.toString();
-    }
+            sb.append("}\n");
+         }
+      }
+      return sb.toString();
+   }
    
 }
-
-
-
